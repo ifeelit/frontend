bece478e19223e447742f975e2f1f76ec081a966
Chaoran Chen <mail@chaoran-chen.de>
2020-03-21 16:45:24 +0100

    Add some fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <app-header></app-header>
-    <router-outlet></router-outlet>
+    <div class="site-content">
+      <main class="main-content">
+        <div class="fullwidth-block"></div>
+        <div class="fullwidth-block">
+          <div class="container">
+            <router-outlet></router-outlet>
+          </div>
+        </div>
+      </main> <!-- .main-content -->
+    </div>
     <app-footer></app-footer>
   `,
   styles: []
