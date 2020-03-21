e9bf029ddbff1c4d1dacf553cf53f492ede7df74
Chaoran Chen <mail@chaoran-chen.de>
2020-03-21 01:16:30 +0100

    Add initial Angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    PIRAT (Pandemic Important Resource Allocation Tool)
+    <router-outlet></router-outlet>
+  `,
+  styles: []
+})
+export class AppComponent {
+
+}
