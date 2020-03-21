40d92f75f5c646bd054ad430b850597836e539e9
Chaoran Chen <mail@chaoran-chen.de>
2020-03-21 08:21:22 +0100

    Update basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { OfferOverviewComponent } from "./offer-overview/offer-overview.component";
-import { DemandOverviewComponent } from "./demand-overview/demand-overview.component";
 import { OfferCreateComponent } from "./offer-create/offer-create.component";
+import { OfferSearchComponent } from "./offer-search/offer-search.component";
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'angebote', component: OfferOverviewComponent },
   { path: 'angebote/neu', component: OfferCreateComponent },
-  { path: 'nachfragen', component: DemandOverviewComponent },
-  { path: 'nachfragen/neu', component: DemandOverviewComponent },
+  { path: 'suchanfrage', component: OfferSearchComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { DemandOverviewComponent } from './demand-overview/demand-overview.component';
-import { OfferOverviewComponent } from './offer-overview/offer-overview.component';
 import { MenuComponent } from './menu/menu.component';
 import { OfferCreateComponent } from './offer-create/offer-create.component';
-import { DemandCreateComponent } from './demand-create/demand-create.component';
+import { OfferSearchComponent } from './offer-search/offer-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DemandOverviewComponent,
-    OfferOverviewComponent,
     MenuComponent,
     OfferCreateComponent,
-    DemandCreateComponent
+    OfferSearchComponent,
   ],
   imports: [
     BrowserModule,
