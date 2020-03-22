e3233d85ac530d81ac7348bce4cd0d50322ce39a
Lukas Vordemann <lukas@vordemann.de>
2020-03-22 13:07:11 +0100

    minor changes

--- src/app/offer-change/offer-change.component.ts
+++ src/app/offer-change/offer-change.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FetchServiceService } from '../fetch-service.service';
+
 
 @Component({
   selector: 'app-offer-change',
@@ -10,8 +12,11 @@ export class OfferChangeComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private key
-  ) { }
+    private key,
+    private fetchService: FetchServiceService
+  ) { };
+
+  goods = [];
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -20,6 +25,54 @@ export class OfferChangeComponent implements OnInit {
       //speicher in goods variable
       //lade seite
     });
+  }
+
+  addPersonnel() {
+    this.goods.push({
+      type: 'personnel',
+      qualification: '',
+      institution: '',
+      researchGroup: '',
+      area: '',
+      experienceWithPCR: undefined,
+      notes: '',
+    });
+  }
+
+
+  addDevice() {
+    this.goods.push({
+      type: 'device',
+      category: '',
+      deviceName: '',
+      manufacturer: '',
+      ordernumber: '',
+      locationPostalCode: '',
+      number: undefined,
+      unit: '',
+      unitSelfDefined: '',
+      notes: '',
+    });
+  }
+
+
+  addConsumable() {
+    this.goods.push({
+      type: 'consumable',
+      category: '',
+      deviceName: '',
+      manufacturer: '',
+      ordernumber: '',
+      locationPostalCode: '',
+      number: undefined,
+      unit: '',
+      unitSelfDefined: '',
+      notes: '',
+    });
+  }
+
+  onSubmit(): void {
+    //DEL request an server through fetch
   }
   
 }
