c91c1c4f60f7a09e569ec704aeb8b5f2707e0b94
Chaoran Chen <mail@chaoran-chen.de>
2020-03-22 09:14:49 +0100

    Ensure that the browser scrolls to the top when opening a new page
    Closes #37

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,16 @@ import { Component } from '@angular/core';
   template: `
   <div class="site-content">
   <app-header></app-header>
-  <router-outlet></router-outlet>
+  <router-outlet (activate)="onActivate($event)"></router-outlet>
   <app-footer></app-footer>
   </div>
   `,
   styles: []
 })
 export class AppComponent {
+
+  onActivate(event) {
+    window.scroll(0,0);
+  }
 
 }
