e76be8c78a0dcbc199630fe7565f1489a246e22a
Lukas Vordemann <lukas@vordemann.de>
2020-03-25 11:44:05 +0100

    Added request form and restyled landing page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchServiceService } from '../fetch-service.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fetchService: FetchServiceService,
+    private router: Router,
+  ) { }
+
+  requestData = {
+    name: '',
+    phone: '',
+    email: '',
+    topic: '',
+    notes: ''
+  };
+
+  recaptcha: string;
+
+
 
   ngOnInit(): void {
+  }
+
+  resolved(captchaResponse) {
+    this.recaptcha = captchaResponse;
+  }
+
+  isValid() {
+
+    let valid = this.requestData.name && this.requestData.phone && this.requestData.topic;
+
+    return valid;
+  }
+
+  onSubmit() {
+    this.sendRequest();
+  }
+
+  async sendRequest() {
+    if (!this.isValid()) {
+      return;
+    }
+
+   this.fetchService.requestCall(this.requestData, this.recaptcha);
+    
+    //ToDo: Show Thank you for the request box
   }
 
 }
