f7b464e488e7ad35073b93159c2802d362f458a2
Chaoran Chen <mail@chaoran-chen.de>
2020-03-28 11:40:27 +0100

    Add tutorial videos

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { environment } from "../../environments/environment";
+import { NavigationEnd, Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 
 @Component({
