4e6bede676c29d6802528794cfd00e8b9cf249cf
Lukas Vordemann <lukas@vordemann.de>
2020-04-06 15:57:56 +0200

    Description Lukas

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -82,10 +82,16 @@ export class AboutComponent implements OnInit {
         en: 'TU Munich - Student (Computer Science)',
       }[this.localeService.locale],
       long: {
-        de: 'Eigentlich wollten Max, Chaoran und ich Kleinbauern in Kenia helfen ihre Marktmacht auszubauen um so' +
-          ' bessere Erträge erzielen zu können.  ...',
-        en: 'Max, Chaoran and I just had started a project in Kenya to help small farmers by improving logistic ' +
-          'chains when the corona crisis hit Europe. ...',
+        de: 'Man kann entweder auf der faulen Haut sitzen und die Krise ausharren oder' + 
+        'sich in irgendeiner Form engagieren und nützlich machen. Hier sind der Kreativität' + 
+        'keine Grenzen gesetzt, was wir auch beim WirVsVirus Hackathon gesehen haben. Die' +  
+        'Arbeit an dem PIRAT Projekt ist für mich nicht nur eine sinnvolle Aufgabe sondern' +  
+        'auch eine einzigartige Erfahrung die ich nicht missen möchte.',
+        en: 'Everybody has the choice to either do nothing and wait until this crisis will ' + 
+        'be over or try to do his part in fighting the virus. There are many ways to contribute ' + 
+        'with your skills to this situation and creativity has no limits, what we have also seen' + 
+        'during the WirVsVirus Hackathon. Working on the PIRAT project has not only been a ' + 
+        'meaningful work to me, but also an unforgettable experience i will never forget.',
       }[this.localeService.locale],
     },
     {
